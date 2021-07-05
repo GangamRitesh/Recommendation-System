4d9919ebb68717051f37add9c73e9735e87722f1
GangamRitesh <riteshreddy717@gmail.com>
2021-07-05 14:46:00 +0530

    Fully formatted

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-var input = $('#upload');
-
-input.change(function (){
-   var filePath = $(this).val();
-   var fileNameArray = filePath.split('\\');
-   var fileName = fileNameArray[fileNameArray.length - 1];
-   $("#filename").text(fileName).show();
-});
